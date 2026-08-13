73c0e28e4e752617c6c9e5f35914c53330e20e4c
leotien-1230 <leotien0906@gmail.com>
2026-08-13 14:01:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "turkey-trip-pwa-v6";
+const CACHE_NAME = "turkey-trip-pwa-v7";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
